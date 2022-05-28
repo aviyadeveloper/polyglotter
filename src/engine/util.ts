4c60bdc1d30997677756141813989c8f7441fad1
aviya.developer <aviya.developer@gmail.com>
2022-05-28 19:54:26 +0200

    fix: util file for engine technical improvements

--- src/engine/util.ts
+++ src/engine/util.ts
@@ -1,18 +1,18 @@
-const fs = require("fs");
+import * as fs from "fs";
 
 const clearDir = (path: string) => {
-  fs.rmdirSync(path + "/", {recursive: true}, simpleErrorBubbler);
+  fs.rmSync(path + "/", {recursive: true});
 };
 
 const makeDir = (path: string) => {
-  fs.mkdirSync(path, {recursive: true}, simpleErrorBubbler);
+  fs.mkdirSync(path, {recursive: true});
 };
 
 export const flushDirSync = (path: string) => {
   path = path.slice(-1) === "/" ? path.slice(0, -1) : path;
   fs.existsSync(`${path}`) && clearDir(path);
   makeDir(path);
-  fs.writeFile(`${path}/_flushed.txt`, Date(), simpleErrorCatcher);
+  fs.writeFileSync(`${path}/_flushed.txt`, Date());
 };
 
 export const simpleErrorBubbler = (error: Error) => {
